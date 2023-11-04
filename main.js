b01524d5ae8fe0261fdd9d5b1f861de489014886
racznandor <racznandor2@gmail.com>
2023-11-04 18:33:22 +0100

    delete item

--- main.js
+++ main.js
@@ -3,28 +3,45 @@ const printToDo = document.querySelector('.printToDo');
 const inputToDo = document.getElementById('toDo');
 const toDoError = document.querySelector('.toDoError');
 
-let toDo = document.createElement('div');
-let deleteButton = document.createElement('i');
-let toDoText = document.createElement('div');
-
 addButton.addEventListener('click', (e) => {
     e.preventDefault();
     if (inputToDo.value) {
-        toDo.classList.add('itemToDo');
-        deleteButton.classList.add('fa-solid', 'fa-xmark', 'fa-xl', 'itemDeleteButton');
-        toDoText.classList.add('itemToDoText');
-
-        toDo.appendChild(toDoText);
-        toDo.appendChild(deleteButton);
-
-        toDoText.innerHTML = '' + inputToDo.value;
-        printToDo.appendChild(toDo);
+        newElement();
         clearModal();
     }
     else {
         toDoError.style.display = 'block';
     }
 });
+
+function newElement() {
+    let toDo = document.createElement('div');
+    let deleteButton = document.createElement('i');
+    let toDoText = document.createElement('div');
+
+    toDo.classList.add('itemToDo');
+    deleteButton.classList.add('fa-solid', 'fa-xmark', 'fa-xl', 'itemDeleteButton');
+    toDoText.classList.add('itemToDoText');
+
+    toDo.appendChild(toDoText);
+    toDo.appendChild(deleteButton);
+
+    toDoText.innerHTML = '' + inputToDo.value;
+    printToDo.appendChild(toDo);
+
+    deleteButton.addEventListener('mouseover', (e) => {
+        deleteButton.classList.add('fa-beat-fade');
+    });
+    
+    deleteButton.addEventListener('mouseout', (e) => {
+        deleteButton.classList.remove('fa-beat-fade');
+    });
+    
+    deleteButton.addEventListener('click', (e) => {
+        let parentElement = e.target.parentElement;
+        parentElement.style.display = 'none';
+    });
+}
 
 function clearModal() {
     toDoError.style.display = 'none';
